0146eeb76bce374daf0b7d4145373eb4a19d21ac
unibitone <80018598+unibitone@users.noreply.github.com>
2025-05-08 16:43:20 +0800

    amnod 1.0.3

--- amax.meta.chain/step3_logs.sh
+++ amax.meta.chain/step3_logs.sh
@@ -1,8 +1,8 @@
 
 # 查看amax节点日志
 
-# 进入 amax_mainnet_0.5.2 数据目录
-cd /opt/data/amax_mainnet_0.5.2
+# 进入 amax_mainnet_1.0.3 数据目录
+cd /opt/data/amax_mainnet_1.0.3
 
 # 查看amnod日志
 tail -f ./logs/amnod.log
